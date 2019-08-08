ccc269c1bf46f13f1e20b61191d486fade88a896
Bruno Abdon <brunoabdon+github@gmail.com>
2019-08-08 06:07:01 -0300

    Revert "Muda forma de atualizar um resource"
    This reverts commit f0cce49ee55441257e054994a75cacaa19aba58e.

--- abd-commons-rest/src/main/java/com/github/brunoabdon/commons/rest/AbstractRestCrud.java
+++ abd-commons-rest/src/main/java/com/github/brunoabdon/commons/rest/AbstractRestCrud.java
@@ -25,6 +25,7 @@ import java.security.SecureRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.NotFoundException;
@@ -120,10 +121,10 @@ public abstract class AbstractRestCrud<E extends Entidade<Key>,Key>
         } else {
 
             try {
-                
-                final E entidadeAtualizar = getDao().find(entityManager, id);
-                this.prencheValoresAtualizacao(entidadeAtualizar,entity);
-                entity = getDao().atualizar(entityManager, entidadeAtualizar);
+
+                entity = prepararAtualizacao(entityManager, entity, id);
+
+                entity = getDao().atualizar(entityManager, entity);
 
                 response = Response.ok(entity).build();
 
@@ -165,10 +166,12 @@ public abstract class AbstractRestCrud<E extends Entidade<Key>,Key>
         return response;
     }
     
-    @SuppressWarnings("unused")
-    protected void prencheValoresAtualizacao(
-            final E entityPraAtualizar, 
-            final E entidadeValoresNovos) {
+    protected E prepararAtualizacao(
+            final EntityManager entityManager, 
+            final E entity, 
+            final Key id) {
+        entity.setId(id);
+        return entity;
     }
 
     private String makeError() {
