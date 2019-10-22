476a647fc77a19c35f22d81b131556e88bf9c8da
Bruno Abdon <brunoabdon+github@gmail.com>
2019-10-22 00:58:50 -0300

    nao precisa ser abstract

--- abd-commons-rest/src/main/java/AbstractResource.java
+++ abd-commons-rest/src/main/java/AbstractResource.java
@@ -1,11 +1,11 @@
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-public abstract class AbstractResource 
+@Path("{id}")
+public class AbstractResource 
                         <E extends ParamInterface<P>, P> {
 
     @GET
-    @Path("{id}")
     public void pegar() {
 
     }
